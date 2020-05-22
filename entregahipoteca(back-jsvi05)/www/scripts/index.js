5648a4a42d6e4ea27c19e09e1376f278439f9a25
raullopezhernando <raullopezhernandorlh@gmail.com>
2020-05-22 21:55:19 +0200

    Mejora y limpieza del codigo

--- entregahipoteca(back-jsvi05)/www/scripts/index.js
+++ entregahipoteca(back-jsvi05)/www/scripts/index.js
@@ -13,6 +13,10 @@ const args = require('yargs')
     .option('mensualidades', {
         alias: 'm'
     })
+    .option('fecha', {
+        alias: 'f',
+        default: moment().format('MMMM Do YYYY, h:mm:ss a')
+    })
     .demandOption('precioDelInmueble', 'El precio del inmueble es obligatorio')
     .demandOption('mensualidades','El numero de mensualidades es obligatorio')
     .argv
@@ -20,6 +24,7 @@ const args = require('yargs')
     let precioDelInmueble = args.precioDelInmueble;
     let tipoInteres = args.tipoInteres;
     let mensualidades = args.mensualidades;
+    let fecha = args.fecha;
 
 
 
@@ -29,7 +34,7 @@ let loan = new Loan({
   pay_every: 'month',
   instalments: mensualidades, // Plazo en años . 12 meses = 1 año
   type: 'annuity',
-  as_of: moment().toDate()
+  as_of: fecha,
 });
 
 
@@ -39,15 +44,15 @@ console.log("-------------------------");
 let amortizacion =
 {
     precio_inmueble : loan.principal,
-    inmueble_mas_intereses : loan.principal + loan.amortize().data.total_cost,
+    inmueble_mas_intereses : loan.principal + loan.amortize().data.total_cost.toFixed(2),
     mensualidades : loan.instalments,
     tipo_plazo : loan.pay_every,
     tipo_interes : loan.amortize().interest_rate * 100 + " %",
-    fecha_inicio : loan.as_of,
+    fecha_inicio : fecha,
     pago_mensual : loan.amortize().to_pay.toFixed(2),
-    amortizacion : loan.amortize().amortization,
-    intereses_mensuales : loan.amortize().interest,
-    intereses_totales : loan.amortize().data.total_cost,
+    amortizacion : loan.amortize().amortization.toFixed(2),
+    intereses_mensuales : loan.amortize().interest.toFixed(2),
+    intereses_totales : loan.amortize().data.total_cost.toFixed(2),
 }
 
 
