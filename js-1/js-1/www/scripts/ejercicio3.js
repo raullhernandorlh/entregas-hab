69e1b41c9703959c185bbcf67c9656c0ed4988f5
raullopezhernando <raullopezhernandorlh@gmail.com>
2020-05-23 11:16:42 +0200

    Revisado el ejercicio3.js (Nuevos cambios)

--- js-1/js-1/www/scripts/ejercicio3.js
+++ js-1/js-1/www/scripts/ejercicio3.js
@@ -5,14 +5,65 @@
 
 
 const axios = require('axios');
-const data = axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then((posts) =>{
-                function getUserMessages(userId){
-                    let userPosts =posts.filter(post => post.userId = userId)
-                    return userPosts
-                }
-                getUserMessages(1)
-            })
+
+// Información de los posts.
+async function getPosts() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+  return response.data;
+}
+
+// Número de posts publicados por un usuario.
+async function countUserMessages(userId) {
+  const posts = await getPosts();
+  return posts.filter((post) => post.userId === userId).length;
+}
+
+// Devolver un objeto con el id del usuario y todos sus post.
+async function getUserAndAllPosts(userId) {
+  const posts = await getPosts();
+  const userPosts = posts.filter((post) => post.userId === userId);
+  const myUser = {
+    userId: userId,
+    posts: []  
+  }
+  for (const post of userPosts) {
+    myUser.posts.push({
+      title: post.title,
+      body: post.body
+    })
+  } 
+  return myUser;
+}
+
+// IIFE.
+(async() => {
+  const result = await (getUserAndAllPosts(1));
+  console.log(result);
+})();
+
+
+
+// Segunda solucion del ejercicio
+
+// const axios = require('axios');
+
+        
+
+
+
+//             function countUserMessages(userId) {
+//               const data = axios.get('https://jsonplaceholder.typicode.com/posts')
+//                 .then((posts) => {
+//                   return posts.data.filter((post) => post.userId === userId)
+//                 })
+//               return data;
+//               // Podríamos hacer aquí un "console.log" de "data" en lugar de "return" y nos ahorramos la IIFE.
+//             }
+            
+//             (async() => {
+//               const result = await (countUserMessages(1));
+//               console.log(result);
+//             })();
         
                 
               
