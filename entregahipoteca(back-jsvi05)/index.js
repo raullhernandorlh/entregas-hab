2fae31a418fed21e2eeab3d8d04d5c0a398979ae
raullopezhernando <raullopezhernandorlh@gmail.com>
2020-05-24 11:08:52 +0200

    Añadida descripcion en index.js "Utilizacion aplicacion y librerias utilizadas"

--- entregahipoteca(back-jsvi05)/index.js
+++ entregahipoteca(back-jsvi05)/index.js
@@ -1,3 +1,26 @@
+// "Simulador de Prestamo Hipoteca"
+
+// Para esta aplicacion he utilizado "Yargs" 
+// https://blog.lungosoft.com/yargs-en-node-pasar-parmetros-en-aplicaciones-javascript-desde-consola
+// Yargs es una libreria con la que podemos parsear parametros de nuestra aplicacion
+// hercha en NodeJS des la consola de comandos
+
+// Para esta aplicacion he utilizado la libreria "Loan"
+// https://www.npmjs.com/package/loan
+
+// EJecutar la aplicacion (Ejecucion y resolucion de la misma en consola)
+// Para ejecutar la aplicacion deberemos poner el precio del inmueble(p) y las
+// mesnusualidades (m)en las que querremos pagarlo. Es posible poner el tipo de interes(i)
+// pero este es opcional. Asi pues procedo a dejar unos ejemplos
+
+// Ejemplo 1 - Precio del Inmueble + Mensualidades + Tipo interes
+// node index --p = 200000 --m =120 --i =0.01
+
+// Ejemplo 2 - Precio del Inmueble + Mensualidades
+// node index --p = 200000 --m =120
+
+
+
 let Loan = require('loan');
 let moment = require('moment');
 
@@ -13,6 +36,10 @@ const args = require('yargs')
     .option('mensualidades', {
         alias: 'm'
     })
+    .option('fecha', {
+        alias: 'f',
+        default: moment().format('MMMM Do YYYY, h:mm:ss a')
+    })
     .demandOption('precioDelInmueble', 'El precio del inmueble es obligatorio')
     .demandOption('mensualidades','El numero de mensualidades es obligatorio')
     .argv
@@ -20,6 +47,7 @@ const args = require('yargs')
     let precioDelInmueble = args.precioDelInmueble;
     let tipoInteres = args.tipoInteres;
     let mensualidades = args.mensualidades;
+    let fecha = args.fecha;
 
 
 
@@ -29,9 +57,8 @@ let loan = new Loan({
   pay_every: 'month',
   instalments: mensualidades, // Plazo en años . 12 meses = 1 año
   type: 'annuity',
-  as_of: moment().toDate()
+  as_of: fecha,
 });
-
 
 
 console.log("Simulador de Hipotecas");
@@ -39,15 +66,15 @@ console.log("-------------------------");
 let amortizacion =
 {
     precio_inmueble : loan.principal,
-    inmueble_mas_intereses : loan.principal + loan.amortize().data.total_cost,
+    inmueble_mas_intereses : (loan.principal + loan.amortize().data.total_cost).toFixed(2),
     mensualidades : loan.instalments,
     tipo_plazo : loan.pay_every,
     tipo_interes : loan.amortize().interest_rate * 100 + " %",
-    fecha_inicio : loan.as_of,
+    fecha_inicio : fecha,
     pago_mensual : loan.amortize().to_pay.toFixed(2),
-    amortizacion : loan.amortize().amortization,
-    intereses_mensuales : loan.amortize().interest,
-    intereses_totales : loan.amortize().data.total_cost,
+    amortizacion : loan.amortize().amortization.toFixed(2),
+    intereses_mensuales : loan.amortize().interest.toFixed(2),
+    intereses_totales : loan.amortize().data.total_cost.toFixed(2),
 }
 
 
